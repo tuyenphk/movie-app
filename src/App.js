cd9348cc419e1ecd7731b913a496880ab7c522aa
Tuyen Pham <tuh39709@temple.edu>
2021-03-10 20:48:48 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import REact from 'react';
+import movie from './assests/movie.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          <tr>
+            <td><img src={movie} alt="movie" className="movie-icon" /></td>
+            <td>Movie Application</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
