a2e6a29d6370634933fdcebe629531efca086e59
Tuyen Pham <tuh39709@temple.edu>
2021-03-10 22:51:17 -0500

    fetch data by jquery

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
 import React from 'react';
-import movie from './assests/movie.png'
+import movie from './assests/movie.svg'
 import './App.css';
 import MovieRow from './MovieRow.js'
+import $ from 'jquery'
 
 class App extends React.Component {
   constructor(props){
     super(props)
+    this.state={}
     
-    const movie=[
-      {
-        id: "0",
-        poster_src: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
-        title: "Avengers: Infinity War",
-        overview: "The Avengers as an organization was emphasized in The Avengers. This film featured Iron Man, Captain America, Hulk and Thor being recruited by Avengers' founder Nick Fury to fight, alongside S.H.I.E.L.D. agents Black Widow and Hawkeye, the villain Loki, who leads an invasion of New York City with a Chitauri army."
-      },
-      {
-        id: '1',
-        poster_src: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
-        title: 'The Avengers',
-        overview: 'The second overview'
-      }
-    ]
+    // const movie=[
+    //   {
+    //     id: "0",
+    //     poster_src: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+    //     title: "Avengers: Infinity War",
+    //     overview: "The Avengers as an organization was emphasized in The Avengers. This film featured Iron Man, Captain America, Hulk and Thor being recruited by Avengers' founder Nick Fury to fight, alongside S.H.I.E.L.D. agents Black Widow and Hawkeye, the villain Loki, who leads an invasion of New York City with a Chitauri army."
+    //   },
+    //   {
+    //     id: '1',
+    //     poster_src: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
+    //     title: 'The Avengers',
+    //     overview: 'The second overview'
+    //   }
+    // ]
 
-    var movieRows=[];
-    movie.forEach((movie)=>{
-      console.log(movie.title)
-      const movieRow = <MovieRow movie={movie} />
-      movieRows.push(movieRow)
-    })
+    // var movieRows=[];
+    // movie.forEach((movie)=>{
+    //   console.log(movie.title)
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
 
-    this.state={rows: movieRows}
+    // this.state={rows: movieRows}
+    this.performSearch();
   }
+
+  performSearch(){
+    console.log("Perform search here...")
+    const urlString="https://api.themoviedb.org/3/search/movie?query=marvel&api_key=1b5adf76a72a13bad99b8fc0c68cb085"
+    $.ajax({
+      url: urlString,
+      success: (searchResults) =>{
+        console.log("Fecthed data successfully")
+      },
+      error(xhr, status, err)  {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
+
   render(){
     return (
       <div className="App">
