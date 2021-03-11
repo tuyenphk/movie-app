bff11e46a3ae0b548c2631ed82c24b42b524b06d
Tuyen Pham <tuh39709@temple.edu>
2021-03-10 21:26:06 -0500

    add search movie bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import REact from 'react';
+import React from 'react';
 import movie from './assests/movie.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <table>
+      <table style={{
+        backgroundColor: '#000',
+        display: 'block',
+        color: '#fff', 
+        fontSize: '30px', 
+        paddingLeft: '16'
+      }}>
         <tbody>
           <tr>
             <td><img src={movie} alt="movie" className="movie-icon" /></td>
-            <td>Movie Application</td>
+            <td>
+              <h3>Movie Search</h3>
+            </td>
           </tr>
         </tbody>
       </table>
+
+      <input placeholder="Enter search movie..." className="input-movie"
+            />
     </div>
   );
 }
